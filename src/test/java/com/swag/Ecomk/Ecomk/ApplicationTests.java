e2b6048f424e6c9689596a90dfb02a5935ddd966
swagnik <swagniksom3@gmail.com>
2026-03-19 22:32:25 +0530

    Initial commit

---
+++ src/test/java/com/swag/Ecomk/Ecomk/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swag.Ecomk.Ecomk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
