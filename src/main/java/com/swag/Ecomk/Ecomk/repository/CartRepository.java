397b06f9f55f9547aad50db8d594c6762754d9d9
swagnik <swagniksom3@gmail.com>
2026-03-25 00:34:15 +0530

    Date-25/03/2026

--- src/main/java/com/swag/Ecomk/Ecomk/repository/CartRepository.java
+++ src/main/java/com/swag/Ecomk/Ecomk/repository/CartRepository.java
@@ -4,4 +4,5 @@ import com.swag.Ecomk.Ecomk.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<CartItem,Long> {
+
 }
